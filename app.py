248c01f8b9c4db1fb6f10fe6d328387c7b0a5bf0
pandyajanakk <pandyajanak5499@gmail.com>
2020-05-17 22:38:50 +0530

    guessing game

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-i = 18
-if i > 30:
-   print (f"i greater than 30 value - > {i}")
-elif i>15:
-    print(f"i between 15 and 30 value - > {i}")
-else:
-    print (f"i less than 15 value - > {i}")
+secret = 9
+i = 1;
+won = 0;
+while (i<=3):
+    guess = int(input('Guess one number '))
+    if guess == secret:
+        print('congratulations')
+        won = 1;
+        break
+    else:
+        i = i+1;
 
-print('I love {} for "{}!"'.format('Geeks', 'Geeks'))
-print('{0} and {1}'.format('Geeks', 'Portal'))
-print('{1} and {0}'.format('Geeks', 'Portal'))
+if(won==0):
+    print('try next time')
+
+
