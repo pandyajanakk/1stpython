94401bb4c6373aa1d97eb0f78361193c92783909
pandyajanakk <pandyajanak5499@gmail.com>
2020-05-17 22:30:59 +0530

    use of if statement + data formatting

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 i = 18
-if (i > 30):
-   print ("i greater than 30")
-elif(i>15):
-    print("i between 15 and 30")
+if i > 30:
+   print (f"i greater than 30 value - > {i}")
+elif i>15:
+    print(f"i between 15 and 30 value - > {i}")
 else:
-    print ("i less than 15")
+    print (f"i less than 15 value - > {i}")
+
+print('I love {} for "{}!"'.format('Geeks', 'Geeks'))
+print('{0} and {1}'.format('Geeks', 'Portal'))
+print('{1} and {0}'.format('Geeks', 'Portal'))
